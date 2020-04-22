57b25b5124008f0a33a3a74e3ca021fbd3d18a4a
Bobkjl <>
2020-04-23 01:56:59 +0800

    Update gost-Utils.sh

--- gost-Utils.sh
+++ gost-Utils.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
-sh_ver="1.2.1"
+sh_ver="1.2.2"
 github="raw.githubusercontent.com/bobkjl/gost-Utils/master"
 
 # 设置字体颜色函数
