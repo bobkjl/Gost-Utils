f256c2ae57cc1bdf641fb705a2f51038b7ebde35
Bobkjl <>
2020-04-25 18:43:14 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 install_gost(){
+	install_tool
     check_git
     git clone https://github.com/bobkjl/Gost-Utils.git
 chmod -R 777 Gost-Utils && cd Gost-Utils && bash gost-Utils.sh
@@ -21,5 +22,20 @@ install_git() {
 	yum install git
     git init
 }
+
+install_tool() {
+    echo "===> Start to install tool"    
+    if [ -x "$(command -v yum)" ]; then
+        command -v curl > /dev/null || yum install -y curl
+        systemctl stop firewalld.service
+        systemctl disable firewalld.service
+    elif [ -x "$(command -v apt)" ]; then
+        command -v curl > /dev/null || apt install -y curl
+    else
+        echo "Package manager is not support this OS. Only support to use yum/apt."
+        exit -1
+    fi 
+}
+
 
 install_gost
